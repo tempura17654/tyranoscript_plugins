d6e38e7c2a1e73fe52d691f5bd2a00ba1a896afa
Ogihara <32210189+tempura17654@users.noreply.github.com>
2018-03-03 06:58:17 +0900

    tempura_cameraプラグイン開発中2

--- data/others/plugin/tempura_camera/_SAMPLE.ks
+++ data/others/plugin/tempura_camera/_SAMPLE.ks
@@ -1,32 +1,84 @@
 [call target=setting]
-
-[plugin name=tempura_camera uselayer=base,0,1,2]
-[tcamera_set z=200,110,90,70 basezoom=1]
-[tcamera time=0]
-
-
 [image layer=base storage=room.jpg time=0]
-[image layer=0    storage=chara/yamato/normal.png x=300 y=000 time=0 name=yamato]
-[image layer=1    storage=chara/mascot/normal.png x=600 y=100 time=0 name=mascot]
-[image layer=2    storage=chara/akane/normal.png  x=000 y=050 time=0 name=akane]
+[chara_show layer=0 name=yamato left=300 top=1   time=0]
+[chara_show layer=1 name=mascot left=600 top=100 time=0]
+[chara_show layer=2 name=akane  left=1   top=50  time=0]
+プラグインを読み込む前。[p]
 
-[tcamera name=akane][l]
-[tcamera name=yamato][l]
-[tcamera name=mascot][l]
-[tcamera x=0 y=0 z=0][l]
 
+
+; プラグインを読み込む。マネージャを有効に。使うレイヤはbase,0,1,2。
+[plugin name=jquery_a3d_update]
+[plugin name=tempura_camera manager=true layer=base,0,1,2 deftime=0]
+
+
+
+プラグインを読み込んだ！　疑似3Dカメラ移動が簡単にできるぞ。[p]
+まずはレイヤー深度の設定とカメラの初期化。
+; それぞれのレイヤのz深度を設定する。base,0,1,2の順に200,120,100,80。数値が大きいほど奥。
+; カメラのz深度とレイヤのz深度の差(＝距離)が100のとき1倍ズームとなる。
+[tcamera_setting z=200,120,100,80]
+; ただしベースレイヤについては特別に2.5倍ズームを基本とする。
+[tcamera_setting zoom=2.5,1,1,1]
+; カメラを初期位置に
+[tcamera_init time=0][p]
+
+
+
+カメラをz=20の位置に。
+[tcamera z=20][p]
+カメラをx=100の位置に。
+[tcamera x=100][p]
+カメラをy=100の位置に。
+[tcamera y=100][p]
+
+
+
+カメラの移動先にキャラクターを指定することもできる！[p]
+あかねが1.5倍ズームで画面中央に映るように移動。
+#akane
+[tcamera name=akane zoom=1.5 y=20][p]
+やまとに移動。
+#yamato
+[tcamera name=yamato][p]
+マスコットに移動。
+#mascot
+[tcamera name=mascot][p]
+
+
+
+#mascot
+キーボードの「C」でカメラマネージャのオン・オフが切り替えられる！[r]
+オン状態になると画面が青枠で囲まれて、ゲームが進まなくなるぞ。[p]
+青枠が出ている状態で画面中央をマウスドラッグするとカメラが動く。[p]
+画面端をマウスドラッグするとカメラが回転する。[r]
+さらにマウスホイールでz深度操作もできる。[p]
+［リセット］をクリックでカメラのリセット、[r]
+タグ文字のところをクリックでタグのコピーができるぞ。[p]
+#mascot
+以上！[p]
+
+#
+[tcamera_init][p]
+[tcamera_setting_init]
+[tcamera_init]
 [s]
-
 
 
 *setting
 [position width=920 height=161 top=451 left=20]
 [position page=fore margint=45 marginl=10 marginr=20 marginb=10 vertical=false opacity=180 color=0x000000]
-[ptext name=chara_name_area layer=message0 color=0xFFFFFF size=26 x=30 y=381 bold=bold]
+[ptext name=chara_name_area layer=message0 color=0xFFFFFF size=26 x=30 y=466 bold=bold]
 [chara_config ptext=chara_name_area]
-[chara_config talk_focus=brightness]
+[chara_config talk_focus=brightness talk_anim=up talk_anim_time=180]
+[button name=role_button role=quicksave graphic=button/qsave.png enterimg=button/qsave2.png x=770 y=470]
+[button name=role_button role=quickload graphic=button/qload.png enterimg=button/qload2.png x=850 y=470]
 [layopt layer=base visible=true]
 [layopt layer=0    visible=true]
 [layopt layer=1    visible=true]
 [layopt layer=2    visible=true]
+[chara_new name=yamato storage=chara/yamato/normal.png jname=やまと]
+[chara_new name=mascot storage=chara/mascot/normal.png jname=？？？]
+[chara_new name=akane  storage=chara/akane/normal.png jname=あかね]
+[delay speed=1]
 [return]
--- data/others/plugin/tempura_camera/tempura_camera.js
+++ data/others/plugin/tempura_camera/tempura_camera.js
@@ -1,188 +1,280 @@
-(function ($, TYRANO, tf, mp) {
+(function ($, TYRANO, mp) {
 
-var layer_names = mp.uselayer.split(",");
+// stat領域に変数定義
+TYRANO.kag.stat.tcamera_memory = {"x": 0, "y": 0, "z": 0, "rotate": 0, "zoom": 1};
+TYRANO.kag.stat.tcamera_layer  = {};
 
-tf.tcamera_param    = [];
-tf.tcamera_index    = 0;
-tf.tcamera_loop     = true;
-tf.tcamera_count    = parseInt(TYRANO.kag.config.numCharacterLayers) + 1;
-tf.tcamera_layer    = [];
-tf.tcamera_pos      = {"x": 0, "y": 0, "z": 0};
-tf.tcamera_bgzoom   = 2;
-tf.tcamera_memory   = {"x": 0, "y": 0, "z": 0};
+// ローカル変数定義
+var tcamera_laynames   = mp.layer.split(","),
+    tcamera_index      = 0,
+    tcamera_loop       = true,
+    tcamera_count      = tcamera_laynames.length,
+    tcamera_param      = {},
+    i,
+    key,
+    newtag = {},
+    SC_WIDTH   = parseInt(TYRANO.kag.config.scWidth),
+    SC_HEIGHT  = parseInt(TYRANO.kag.config.scHeight),
+    DEF_TIME   = String(mp.deftime || 1000),
+    SKIP_TIME  = String(mp.skiptime || 0),
+    BASE_LAYER = mp.base || "base",
+    ZOOM_MAX   = 8,
+    DIF_Z_RATE = 100,
+    DIF_Z_MIN  = parseInt(DIF_Z_RATE / ZOOM_MAX),
+    getMemory = function () {
+        return TYRANO.kag.stat.tcamera_memory;
+    },
+    getLayer = function () {
+        return TYRANO.kag.stat.tcamera_layer;
+    }
+    getLayerName = function (num) {
+        return tcamera_laynames[num];
+    },
+    getLayerNameJ = function (j_layer) {
+        var ret, i, list = j_layer.get(0).className.split(/\s+/);
+        for (i = 0; i < list.length; i++) {
+            if (list[i].indexOf("_fore") > -1 && list[i].indexOf("layer_") < 0) {
+                ret = list[i].replace("_fore", "");
+                break;
+            }
+        }
+        return ret;
+    },
+    initLayers = function () {
+        var lay_obj = getLayer();
+        for (i = 0; i < tcamera_count; i++) {
+           key = getLayerName(i);
+           lay_obj[key] = {
+              "x": 0,
+              "y": 0,
+              "z": 100,
+              "zoom": 1,
+           };
+        }
+    };
+    initLayers();
 
-var SC_WIDTH = parseInt(TYRANO.kag.config.scWidth);
-var SC_HEIGHT = parseInt(TYRANO.kag.config.scHeight);
-
-//# getLayerName
-var getLayerName = function (num) {
-    num = parseInt(num);
-    if (! num) return "base";
-    else return "" + (num - 1);
+//# パラメータ自動計算
+var calcCamera = function (pm) {
+    var i, key, val, dif_z, pos_obj, cam_obj, zoom,
+    j_chara, j_parent, target_x, target_zoom, chara_left, chara_width, chara_layer;
+    
+    // pmについて、数値型に変換できるプロパティはすべて数値型にする
+    for (key in pm) {
+        val = parseFloat(pm[key]);
+        if (! isNaN(val)) {
+            pm[key] = val;
+        }
+    }
+    
+    // pm.zは絶対必要
+    if (pm.z === "") {
+        pm.z = getMemory().z;
+    }
+    
+    // name指定があれば
+    if (pm.name !== "") {
+        // pm.zoomは絶対必要
+        if (pm.zoom === "") {
+            pm.zoom = getMemory().zoom;
+        }
+        // キャラクターのjQueryオブジェクトを取得
+        j_chara = pm.chara;
+        // 親レイヤーのjQueryオブジェクトを取得
+        j_parent = j_chara;
+        for (i = 0; i < 3; i++) {
+            j_parent = j_parent.parent();
+            if (j_parent.hasClass("layer")) break;
+        }
+        // 親レイヤーの名前を取得
+        chara_layer = getLayerNameJ(j_parent);
+        //console.log(chara_layer);
+        // widthとleftを元に目標xを決定する
+        chara_width = j_chara.width() || 0;
+        chara_left  = parseInt(j_chara.css("left")) || 0;
+        target_x    = chara_left + chara_width / 2 - SC_WIDTH / 2;
+    }
+    
+    // レイヤーの数だけループ…するのだが
+    i = (pm.name !== "") ? -1 : 0; // name指定がある場合は-1からスタート
+    for (; i < tcamera_count; i++) {
+        // -1の場合はキャラレイヤーを参照
+        key = i < 0 ? chara_layer : getLayerName(i);
+        // レイヤーのposを参照
+        pos_obj = getLayer()[key];
+        // -1の場合はpmを補正する
+        if (i < 0) {
+            target_zoom = Math.min(pm.zoom, ZOOM_MAX);
+            pm.x = target_x / target_zoom;
+            pm.z = pos_obj.z - DIF_Z_RATE / target_zoom;
+            continue;
+        }
+        // 0以上ならパラメータ作成
+        else {
+            // 新規オブジェクト
+            cam_obj = {
+                "layer": key,
+                "wait": "false",
+                "ease_type": pm.ease_type,
+                "time": String(TYRANO.kag.stat.is_skip === true ? SKIP_TIME : pm.time),
+            };
+            // 計算
+            dif_z      = pos_obj.z - pm.z;           // レイヤーとカメラのz距離
+            is_visible = dif_z > 0;                  // カメラより前にある物だけ見える(未使用)
+            dif_z      = Math.max(dif_z, DIF_Z_MIN); // z距離に最低値を保証する
+            zoom       = DIF_Z_RATE / dif_z;         // 拡大率はz距離に反比例する
+            // パラメータを設定していく
+            if (pm.x !== "") cam_obj.x = String(pm.x * zoom);
+            if (pm.y !== "") cam_obj.y = String(pm.y * zoom);
+            if (pm.rotate !== "") cam_obj.rotate = String(pm.rotate);
+            cam_obj.zoom = String(zoom * pos_obj.zoom);
+            //console.log(cam_obj);
+            tcamera_param[key] = cam_obj;
+        }
+    }
 };
 
-var i, key;
-for (i = 0; i < tf.tcamera_count; i++) {
-   key = getLayerName(i);
-   tf.tcamera_layer[i] = {
-      "x": 0,
-      "y": 0,
-      "z": 0
-   };
-   tf.tcamera_param[i] = {
-      "layer": layer_names[i],
-   };
-}
-
-var newtag = {};
-
-//# [tcamera_set]
-newtag.tcamera_set = {
+//# 深度設定
+newtag.tcamera_setting = {
     pm: {
         z: "",
-        bgzoom: ""
+        zoom: ""
     },
     start: function (pm) {
-        var zs, i, val;
+        var zs, zooms, i, val, key;
         if (pm.z !== "") {
             zs = pm.z.split(",");
             for (i = 0; i < zs.length; i++) {
-                val = parseInt(zs[i]);
-                tf.tcamera_layer[i].z = val;
+                if (zs[i] !== "") {
+                    key = getLayerName(i);
+                    val = parseInt(zs[i]);
+                    getLayer()[key].z = val;
+                }
             }
         }
-        if (pm.bgzoom !== "") {
-            tf.tcamera_bgzoom = parseInt(pm.bgzoom);
+        if (pm.zoom !== "") {
+            zooms = pm.zoom.split(",");
+            for (i = 0; i < zooms.length; i++) {
+                if (zooms[i] !== "") {
+                    key = getLayerName(i);
+                    val = parseFloat(zooms[i]);
+                    getLayer()[key].zoom = val;
+                }
+            }
         }
         this.kag.ftag.nextOrder();
     }
 };
 
-
-var ZOOM_MAX = 10;
-var DIF_Z_RATE = 100;
-var DIF_Z_MIN = 1 / ZOOM_MAX;
-
-//# calcCamera
-var calcCamera = function (pm) {
-    var i, _i, key, val, dif_z, pos_obj, cam_obj, zoom,
-    j_chara, j_parent, chara_x, chara_left, chara_width, chara_layer = -1;
-    // 数値型に変換できるプロパティは数値型にする
-    for (key in pm) {
-        val = parseInt(pm[key]);
-        if (! isNaN(val)) {
-            pm[key] = val;
-        }
-    }
-    if (pm.name !== "") {
-        j_chara = pm.chara;
-        j_parent = j_chara;
-        for (var i = 0; i < 3; i++) {
-            j_parent = j_parent.parent();
-            if (j_parent.hasClass("layer")) break;
-        }
-        chara_layer = parseInt(j_parent.zIndex()) || 0;
-        if (chara_layer >= 10) {
-            chara_layer = chara_layer - 9;
-        }
-        chara_width = j_chara.width() || 0;
-        chara_left  = parseInt(j_chara.css("left")) || 0;
-        chara_x     = chara_left + chara_width / 2 - SC_WIDTH / 2;
-    }
-    // レイヤーの数だけループ
-    i = pm.name ? -1 : 0;
-    for (; i < tf.tcamera_count; i++) {
-        _i = i < 0 ? chara_layer : i;
-        // 参照
-        pos_obj = tf.tcamera_layer[_i];
-        cam_obj = tf.tcamera_param[_i];
-        // レイヤーとカメラ間のz距離を計算する
-        dif_z      = pos_obj.z - pm.z;
-        is_visible = dif_z > 0;                  // カメラより後ろにある物は見えない
-        dif_z      = Math.max(dif_z, DIF_Z_MIN); // 最低値を保証する
-        zoom       = DIF_Z_RATE / dif_z;         // 拡大率は距離に反比例する
-        if (i < 0) {
-            pm.x = chara_x;
-            pm.z = pos_obj.z - 100;
-        }
-        else {
-            // パラメータの初期化
-            cam_obj.x    = pm.x;
-            cam_obj.y    = pm.y;
-            cam_obj.zoom = 1;
-            cam_obj.time = pm.time;
-            // パラメータを計算していく
-            cam_obj.zoom *= zoom;
-            cam_obj.x    *= zoom;
-            cam_obj.y    *= zoom;
-            if (i === 0) {
-                cam_obj.zoom += tf.tcamera_bgzoom;
-            }
-            //console.log(cam_obj.x);
-            for (key in cam_obj) {
-              cam_obj[key] = String(cam_obj[key]);
-            }
-        }
-    }
+//# 深度設定初期化
+newtag.tcamera_setting_init = {
+    start: function (pm) {
+        initLayers();
+        this.kag.ftag.nextOrder();
+    },
 };
 
-//# [tcamera]
+//# てんぷらカメラ初期化
+newtag.tcamera_init = {
+    start: function (pm) {
+        pm.x = "0";
+        pm.y = "0";
+        pm.z = "0";
+        pm.rotate = "0";
+        this.kag.ftag.startTag("tcamera", pm);
+    },
+};
+
+//# てんぷらカメラ
 newtag.tcamera = {
     pm: {
-        "x": "0",
-        "y": "0",
-        "z": "0",
+        "x": "",
+        "y": "",
+        "z": "",
+        "rotate": "",
         "name": "",
-        "time": "1000",
+        "zoom": "",
+        "wait": "true",
+        "ease_type": "ease",
+        "time": DEF_TIME,
     },
     start: function (pm) {
-        var that = this;
+        var that = this, key, memory, cam_obj;
+        
+        // もしname指定があって、pm.charaが未定義なら
         if (pm.name !== "" && ! pm.chara) {
-            pm.chara = $("." + pm.name);
+            // そのキャラのjQueryオブジェクトを取得してpm.charaに格納する
+            pm.chara = $("." + pm.name).eq(0);
+            // もしそのキャラが<img>要素であり、かつ、画像の読み込みが終わっていないならば
             if (pm.chara.get(0).tagName === "IMG" && ! pm.chara.get(0).complete) {
-                pm.chara.on("load", function () {
+                // 画像の読み込みが終わったときにもう一度この関数をやり直す
+                return pm.chara.on("load", function () {
                     that.start(pm);
                 });
-                return;
             }
         }
-        if (tf.tcamera_index === 0) {
+        
+        // 最初のカメラなら
+        if (tcamera_index === 0) {
+            // pmを用いてカメラ用のパラメータを計算する
             calcCamera(pm);
-            tf.tcamera_memory = pm;
+            // カメラ位置を覚えておく
+            memory = getMemory();
+            for (key in memory) {
+                if (pm[key] !== "") {
+                    memory[key] = pm[key];
+                }
+            }
             //console.log(pm);
+            //console.log($.extend({}, tcamera_param));
         }
-        var cam_obj = tf.tcamera_param[tf.tcamera_index];
-        tf.tcamera_index++;
-        tf.tcamera_loop = tf.tcamera_index < tf.tcamera_count;
+        
+        // カメラパラメータを取得する
+        key = getLayerName(tcamera_index);
+        cam_obj = tcamera_param[key];
+        // インデックス増加、ループするかどうかを判定
+        tcamera_index++;
+        tcamera_loop = tcamera_index < tcamera_count;
+        
+        //【特殊】クイック処理ならば
         if (pm.quick) {
+            // 専用のタグでカメラを動かす
             quickMoveCamera(cam_obj);
-            if (tf.tcamera_loop) {
-                this.start(pm);
+            // ループするなら再帰する
+            if (tcamera_loop) {
+                return this.start(pm);
             }
+            // ループしないならインデックスを初期化して終了
             else {
-                tf.tcamera_index = 0;
+                return tcamera_index = 0;
             }
         }
+        
+        // 通常のタグによる処理ならば
         else {
-            if (tf.tcamera_loop) {
+            // ループするならオーダーを戻す
+            if (tcamera_loop) {
                 this.kag.ftag.current_order_index--;
-                cam_obj.wait = "false";
             }
+            // ループしないならインデックスを初期化
             else {
-                tf.tcamera_index = 0;
-                cam_obj.wait = "true";
+                tcamera_index = 0;
+                cam_obj.wait = pm.wait;
             }
+            // 正規の[camera]に処理を投げる
+            //console.log(cam_obj);
             this.kag.ftag.startTag("camera", cam_obj);
         }
     }
 };
 
-for (var key in newtag) {
+// 新タグを登録
+for (key in newtag) {
     newtag[key].kag = TYRANO.kag;
     TYRANO.kag.ftag.master_tag[key] = newtag[key];
 }
 
-
+//# カメラを一瞬で動かす
 var quickMoveCamera = function (pm) {
     pm = $.extend(TYRANO.kag.tag.camera.pm, pm);
     var that = TYRANO.kag.ftag.master_tag.camera;
@@ -206,9 +298,10 @@ var quickMoveCamera = function (pm) {
             scale : pm.from_zoom,
             rotate:pm.from_rotate+"deg"
         };
-    };
+    }
     //console.log(pm.layer + ": " + to_camera.scale);
-    var css_str = "scale(" + to_camera.scale + ") rotate(" + to_camera.rotate + ") translate3d(" + to_camera.x + "," + to_camera.y + ",0px)";
+    //var css_str = "scale(" + to_camera.scale + ") rotate(" + to_camera.rotate + ") translate3d(" + to_camera.x + "," + to_camera.y + ",0px)";
+    var css_str = "scale(" + to_camera.scale + ") rotate(" + to_camera.rotate + ") translate(" + to_camera.x + "," + to_camera.y + ")";
     //console.log(css_str);
     if(pm.layer=="layer_camera"){
         $(".layer_camera").css({
@@ -227,33 +320,127 @@ var quickMoveCamera = function (pm) {
     }
 };
 
-var id = ".tcamera";
-var isMousedown = false;
-var startX = -1;
-var startY = -1;
-var camera = {};
-$(window).on("mousedown" + id, function (e) {
-    isMousedown = true;
-    startX = e.pageX;
-    startY = e.pageY;
-    camera = $.extend({}, tf.tcamera_memory);
-});
-$(window).on("mousemove" + id, function (e) {
-    if (! isMousedown) return;
-    var dx  = e.pageX - startX,
-        dy  = e.pageY - startY;
-    dx *= -1;
-    newtag.tcamera.start($.extend({}, newtag.tcamera.pm, {
-        x: parseInt(camera.x) + dx,
-        y: parseInt(camera.y) + dy,
-        z: parseInt(camera.z),
-        time: "0",
-        quick: true
-    }));
-   
-});
-$(window).on("mouseup" + id, function (e) {
-    isMousedown = false;
-});
+//# マネージャ
+// マネージャが有効じゃなければここで終わり
+if (mp.manager !== "true") {
+    return;
+}
+// 有効ならマネージャの作成
+else {
+    var enableAutoCopy = document.execCommand("copy"),
+        isOnSpan = false,
+        cameraMode = 0,
+        startX = -1,
+        startY = -1,
+        sign   = 1,
+        camera = {},
+        opt    = {"x": 0, "y": 0, "z": 0, "rotate": 0, "quick": true},
+        j_side,
+        j_span = $("<span style=\"z-index: 1; display: inline-block; position: absolute; cursor: default; font-family: " + TYRANO.kag.config.userFace + "; font-size: 16px; color: white; background: blue\"></span>"),
+        j_span1 = $("<span>カメラ制御モード</span>"),
+        j_span2 = $("<span style=\"cursor: pointer\">[リセット]</span>"),
+        j_span3 = $("<span style=\"cursor: pointer\"></span>"),
+        j_left  = $("<div data-side=\"left\" style=\"cursor: ns-resize; z-index: 0; position: absolute;  left: 0; top: 0; bottom: 0; width: 80px;\"></div>"),
+        j_right = $("<div data-side=\"right\" style=\"cursor: ns-resize; z-index: 0; position: absolute; right: 0; top: 0; bottom: 0; width: 80px;\"></div>"),
+        j_pre = $("<pre></pre>"),
+        j_copy = $("<div style=\"position: fixed; left: -100%;\"></div>"),
+        j_event = $("<div id=\"tcamera_manager\" style=\"cursor: move; z-index: 100000010; position: absolute; width: "+SC_WIDTH+"px; height: "+SC_HEIGHT+"px; box-sizing: border-box; border: 5px solid blue; display: none;\"></div>"),
+        makeTag = function (opt) {
+            j_span3.text("[tcamera x=" + opt.x + " y=" + opt.y + " z=" + opt.z + " rotate=" + opt.rotate + "]");
+            if (enableAutoCopy) j_span3.trigger("click");
+        };
+        
+    $("#tyrano_base").append(j_event);
+    j_span.append(j_span1).append(j_span2).append(j_span3);
+    j_event.append(j_span).append(j_right).append(j_left);
+    j_copy.append(j_pre);
+    $("body").append(j_copy);
+    $(window).on("keydown", function (e) {
+        if (e.keyCode === 67) {
+            j_event.toggle();
+        }
+    });
+    j_span2.on("click", function (e) {
+        opt.x = 0;
+        opt.y = 0;
+        opt.z = 0;
+        opt.rotate = 0;
+        makeTag(opt);
+        newtag.tcamera.start($.extend({}, newtag.tcamera.pm, opt));
+        e.stopPropagation();
+    });
+    j_span3.on("click", function (e) {
+        j_pre.get(0).textContent = j_span3.text();
+        document.getSelection().selectAllChildren(j_copy.get(0));
+        document.execCommand("copy");
+        e.stopPropagation();
+    });
+    j_event.on("onwheel" in document ? "wheel" : "onmousewheel" in document ? "mousewheel" : "DOMMouseScroll", function (e) {
+        var delta = e.originalEvent.deltaY ? -(e.originalEvent.deltaY) : e.originalEvent.wheelDelta ? e.originalEvent.wheelDelta : -(e.originalEvent.detail);
+        camera = $.extend({}, getMemory());
+        opt.x = camera.x;
+        opt.y = camera.y;
+        opt.z = camera.z;
+        if (delta < 0){
+            opt.z = parseInt(opt.z - 5);
+        } else {
+            opt.z = parseInt(opt.z + 5);
+        }
+        makeTag(opt);
+        newtag.tcamera.start($.extend({}, newtag.tcamera.pm, opt));
+        e.stopPropagation();
+    });
+    j_side = j_left.add(j_right);
+    j_side.on("mousedown", function (e) {
+        if (isOnSpan) return;
+        sign = $(this).attr("data-side") === "left" ? -1 : 1;
+        cameraMode = 2;
+        startX = e.pageX;
+        startY = e.pageY;
+        camera = $.extend({}, getMemory());
+        e.stopPropagation();
+    });
+    j_event.on("mousedown", function (e) {
+        if (isOnSpan) return;
+        cameraMode = 1;
+        j_side.hide();
+        startX = e.pageX;
+        startY = e.pageY;
+        camera = $.extend({}, getMemory());
+        e.stopPropagation();
+    });
+    $(window).on("mousemove", function (e) {
+        if (! cameraMode) return;
+        var dx  = e.pageX - startX,
+            dy  = e.pageY - startY;
+        if (cameraMode === 1) {
+            dx *= -1;
+            opt.x = parseInt(camera.x || 0) + dx;
+            opt.y = parseInt(camera.y || 0) + dy;
+            opt.z = parseInt(camera.z || 0);
+        }
+        else if (cameraMode === 2) {
+            dy *= sign;
+            opt.rotate = parseInt(camera.rotate || 0) + parseInt(dy / 2);
+        }
+        makeTag(opt);
+        newtag.tcamera.start($.extend({}, newtag.tcamera.pm, opt));
+        e.stopPropagation();
+    });
+    $(window).on("mouseup", function (e) {
+        if (cameraMode) {
+            cameraMode = 0;
+            j_side.show();
+            e.stopPropagation();
+        }
+    });
+    j_span.on("mousedown", function (e) {
+        isOnSpan = true;
+    });
+    j_span.on("mouseup", function (e) {
+        isOnSpan = false;
+        e.stopPropagation();
+    });
+}
 
-}(window.jQuery, window.TYRANO, window.TYRANO.kag.variable.tf, window.TYRANO.kag.stat.mp));
+}(window.jQuery, window.TYRANO, window.TYRANO.kag.stat.mp));
